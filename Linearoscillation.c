7d41dc0767c6a0bc6470e1d8e8c72545c9257413
arya-ht <aryaht23@gmail.com>
2017-07-17 17:51:51 -0500

    uploaded file
    This program calculates a simple linear oscillator module.

---
+++ Linearoscillation.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define PI 3.14159265358979323846
+int main() {
+	double omega = 0.01, dt = 0.01, posX = 10.0, velocity = 0.0, time = 0.0;
+	double gravity, length;
+	double tmpx = 10.0, tmpvelocity = 0.0;
+
+	while (time < (4 * PI / omega)) {
+		posX = tmpx + dt*tmpvelocity;
+		velocity = tmpvelocity - pow(omega, 2)*tmpx;
+		tmpx = posX;
+		tmpvelocity = velocity;
+		time += dt;
+	}
+
+	printf("time= %lf posX= %lf velocity= %lf\n", time, posX, velocity);
+	printf("\n");
+	system("pause");
+	return 0;
+}
